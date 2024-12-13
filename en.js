996b002200c72b916b3e01b88d5f02bed0554122
Daniela <daniela.jurado@dechema.de>
2024-12-13 13:57:03 +0100

    ref5

--- en.js
+++ en.js
@@ -39,7 +39,7 @@ var translations_en = {
   data_title: `Data and sources`,
   methods: `<h2>Sources and Methodology</h2>
   This map is based on information from South Africa’s Department of Forestry, Fisheries and the Environment, as presented in the 9th National Greenhouse Gas Inventory Report for the Republic of South Africa [1], and the published study Carbon Sources for PtX Products and Synthetic Fuels in South Africa [2]. Using this information, along with further estimates derived from public sources and industry-specific information, CO₂ emissions allocations were determined as detailed below.<br>
-  <b>Coal-Fired Power Stations<b>
+  <b>Coal-Fired Power Stations</b>
   <br>The total energy production values for each power station were obtained from Eskom’s Atmospheric Emission License (AEL) reports [3]. Using Eskom’s total annual CO₂ emissions [4] and total electricity produced, an emission factor of 1.1 was estimated. Based on this factor and the specific electricity production at each power station, the CO₂ emission allocation at each generation point was calculated.
   <br><b>Petrochemical</b>
   <br>Major CO₂ emitters in South Africa’s petrochemical industry are associated with Sasol's facilities, with emission values sourced from Sasol’s Climate Change Report [5]. 
